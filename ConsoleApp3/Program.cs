f1863b00afdff4a480522a4a824a9ca5a3392491
Bruno Capuano <bcapuano@gmail.com>
2025-07-24 13:50:25 -0400

    Add ConsoleApp3 for text-to-image generation
    A new project, `ConsoleApp3`, has been added to the solution, including configurations for Debug and Release builds. The project targets .NET 9.0 and references the `TransformersSharp` project. It includes a `Program.cs` file that utilizes the `TransformersSharp.Pipelines` namespace to create a text-to-image pipeline, generating an image from a text prompt and saving it to the desktop.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using TransformersSharp.Pipelines;
+
+var pipeline = TextToImagePipeline.FromModel(
+    "kandinsky-community/kandinsky-2-2-decoder",
+    trustRemoteCode: true);
+
+var result = pipeline.Generate(
+    "A pixelated image of a beaver in Canada",
+    numInferenceSteps: 30,
+    guidanceScale: 7.5f,
+    height: 512,
+    width: 512);
+
+var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+var folderPath = Path.Combine(desktopPath, "TransformersSharpImages");
+Directory.CreateDirectory(folderPath); 
+var destinationPath = Path.Combine(folderPath, "image.png");
+File.WriteAllBytes(destinationPath, result.ImageBytes); 
