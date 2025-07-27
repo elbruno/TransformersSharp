1b3e694c3d103b5964210aca009f731446efaf47
Bruno Capuano <bcapuano@gmail.com>
2025-07-27 13:50:32 -0400

    Implement code changes to enhance functionality and improve performance

--- python_tests/test_kandinsky_2_2.py
+++ python_tests/test_kandinsky_2_2.py
@@ -2,12 +2,17 @@ from diffusers import AutoPipelineForText2Image
 import torch
 
 
-pipe = AutoPipelineForText2Image.from_pretrained("kandinsky-community/kandinsky-2-2-decoder", torch_dtype=torch.float16)
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device.upper()}")
+if device == "cuda":
+	pipe = AutoPipelineForText2Image.from_pretrained("kandinsky-community/kandinsky-2-2-decoder", torch_dtype=torch.float16)
+else:
+	pipe = AutoPipelineForText2Image.from_pretrained("kandinsky-community/kandinsky-2-2-decoder")
 pipe = pipe.to(device)
 
 prompt = "portrait of a young women, blue eyes, cinematic"
 negative_prompt = "low quality, bad quality"
 
-image = pipe(prompt=prompt, negative_prompt=negative_prompt, prior_guidance_scale=1.0, height=256, width=256).images[0]
+image = pipe(prompt=prompt, negative_prompt=negative_prompt, prior_guidance_scale=0.75, height=256, width=256).images[0]
 image.save("portrait.png")
